25bde0b1f4053e5817a0019f83552b553c96c271
martincalsina <martincalsina05@gmail.com>
2023-08-16 15:20:21 -0300

    Interfaz basica cargada

---
+++ src/main/java/com/ar/alura/challene/hotel/alura/ChalleneHotelAlura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ar.alura.challene.hotel.alura;
+
+/**
+ *
+ * @author marti
+ */
+public class ChalleneHotelAlura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
